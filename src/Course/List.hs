278b898e1b540f773a59e1e9e40008be5356fd1b
William Burns <github@william-burns.com>
2025-05-12 09:57:50 -0400

    Implement List

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -76,8 +76,8 @@ headOr ::
   a
   -> List a
   -> a
-headOr =
-  error "todo: Course.List#headOr"
+headOr a Nil = a
+headOr _ (a:._) = a
 
 -- | The product of the elements of a list.
 --
@@ -92,8 +92,8 @@ headOr =
 product ::
   List Int
   -> Int
-product =
-  error "todo: Course.List#product"
+product Nil = 1
+product (x:.xs) = x * (product xs)
 
 -- | Sum the elements of the list.
 --
@@ -107,8 +107,8 @@ product =
 sum ::
   List Int
   -> Int
-sum =
-  error "todo: Course.List#sum"
+sum Nil = 0
+sum (x:.xs) = x + (sum xs)
 
 -- | Return the length of the list.
 --
@@ -119,8 +119,8 @@ sum =
 length ::
   List a
   -> Int
-length =
-  error "todo: Course.List#length"
+length Nil = 0
+length (x:.xs) = 1 + (length xs)
 
 -- | Map the given function on each element of the list.
 --
@@ -134,8 +134,8 @@ map ::
   (a -> b)
   -> List a
   -> List b
-map =
-  error "todo: Course.List#map"
+map _ Nil = Nil
+map f (x:.xs) = (f x) :. (map f xs)
 
 -- | Return elements satisfying the given predicate.
 --
@@ -151,8 +151,10 @@ filter ::
   (a -> Bool)
   -> List a
   -> List a
-filter =
-  error "todo: Course.List#filter"
+filter _ Nil = Nil
+filter f (x:.xs) = if f x
+                   then x :. (filter f xs)
+                   else filter f xs
 
 -- | Append two lists to a new list.
 --
@@ -170,8 +172,8 @@ filter =
   List a
   -> List a
   -> List a
-(++) =
-  error "todo: Course.List#(++)"
+(++) Nil ys = ys
+(++) (x:.xs) ys = x :. (xs ++ ys)
 
 infixr 5 ++
 
@@ -188,8 +190,8 @@ infixr 5 ++
 flatten ::
   List (List a)
   -> List a
-flatten =
-  error "todo: Course.List#flatten"
+flatten Nil = Nil
+flatten (x:.xs) = x ++ (flatten xs)
 
 -- | Map a function then flatten to a list.
 --
@@ -205,8 +207,7 @@ flatMap ::
   (a -> List b)
   -> List a
   -> List b
-flatMap =
-  error "todo: Course.List#flatMap"
+flatMap f xs = flatten $ map f xs
 
 -- | Flatten a list of lists to a list (again).
 -- HOWEVER, this time use the /flatMap/ function that you just wrote.
@@ -215,8 +216,7 @@ flatMap =
 flattenAgain ::
   List (List a)
   -> List a
-flattenAgain =
-  error "todo: Course.List#flattenAgain"
+flattenAgain = flatMap id
 
 -- | Convert a list of optional values to an optional list of values.
 --
@@ -240,8 +240,12 @@ flattenAgain =
 seqOptional ::
   List (Optional a)
   -> Optional (List a)
-seqOptional =
-  error "todo: Course.List#seqOptional"
+seqOptional Nil = Full Nil
+seqOptional (Empty:._) = Empty
+seqOptional ((Full x):.xs) = let rest = seqOptional xs
+                             in case rest of
+                                Empty -> Empty
+                                (Full ys) -> Full (x :. ys)
 
 -- | Find the first element in the list matching the predicate.
 --
@@ -263,8 +267,10 @@ find ::
   (a -> Bool)
   -> List a
   -> Optional a
-find =
-  error "todo: Course.List#find"
+find _ Nil = Empty
+find f (x:.xs) = if f x
+                 then Full x
+                 else find f xs
 
 -- | Determine if the length of the given list is greater than 4.
 --
@@ -282,8 +288,11 @@ find =
 lengthGT4 ::
   List a
   -> Bool
-lengthGT4 =
-  error "todo: Course.List#lengthGT4"
+lengthGT4 = helper 0
+  where helper n Nil = n > 4
+        helper n (_:.xs) = if (n > 4)
+                           then True
+                           else helper (n + 1) xs
 
 -- | Reverse a list.
 --
@@ -299,8 +308,9 @@ lengthGT4 =
 reverse ::
   List a
   -> List a
-reverse =
-  error "todo: Course.List#reverse"
+reverse xs = foldLeft (flip (:.)) Nil xs
+-- reverse Nil = Nil
+-- reverse (x:.xs) = (reverse xs) ++ (x :. Nil)
 
 -- | Produce an infinite `List` that seeds with the given value at its head,
 -- then runs the given function for subsequent elements
@@ -314,8 +324,7 @@ produce ::
   (a -> a)
   -> a
   -> List a
-produce f x =
-  error "todo: Course.List#produce"
+produce f x = x :. (produce f (f x))
 
 -- | Do anything other than reverse a list.
 -- Is it even possible?
